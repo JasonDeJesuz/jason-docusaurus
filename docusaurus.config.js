3edc80a35d936991a7ec814342b262614a61e12f
Jason De Jesuz <dejesuzjason@gmail.com>
2025-03-07 14:13:16 +0800

    nice

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -137,10 +137,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/jasondejesuz',
